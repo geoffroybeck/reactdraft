b0dffcac6c3e58fbe54728a61e5661b551bba514
geoffroy@toktokdoc.com <geoffroy@toktokdoc.com>
2020-06-20 17:37:57 +0200

    now we have a nice little form, with an ugly form below

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
+import Input from "@material-ui/core/Input";
+import { Input as InputField } from "antd";
+import Checkout from "./Checkout"
 
 function App() {
+  const { control, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Checkout></Checkout>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller as={Input} name="HelloWorld" control={control} defaultValue="" />
+      <Controller as={InputField} name="AntdInput" control={control} defaultValue="" />
+      <Controller
+        as={Select}
+        name="reactSelect"
+        control={control}
+        onChange={([selected]) => {
+          // React Select return object instead of value for selection
+          return { value: selected };
+        }}
+        defaultValue={{}}
+      />
+
+      <input type="submit" />
+    </form>
     </div>
   );
 }
+
 
 export default App;
